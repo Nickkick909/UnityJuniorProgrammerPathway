ba483ed7eef1e0d2e0362b94eb08873be088244e
Nicholas Knight <nicholasknight1999@yahoo.com>
2024-11-13 14:15:12 -0500

    Finishing prototype one and creating and importing assets for prototype 2

--- Prototype 1/Assets/Scripts/CameraFollow.cs
+++ Prototype 1/Assets/Scripts/CameraFollow.cs
@@ -4,6 +4,7 @@ public class CameraFollow : MonoBehaviour
 {
     [SerializeField] Transform playerTransform;
     [SerializeField] Vector3 offset;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,11 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    public float moveSpeed;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float turnSpeed;
+
+    float horizontalInput;
+    float verticalInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +16,18 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+        CheckForInput();
+        
+    }
+
+    void CheckForInput()
+    {
+        // Forward or reverse
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed * verticalInput);
+
+        // Turning left or right
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
